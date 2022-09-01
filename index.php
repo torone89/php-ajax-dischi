0ecae50a5662f7ca43a9f940a264a0976125d11a
torone89 <torone89>
2022-09-01 16:49:40 +0200

    collego Vue al foglio html e monto e richiamo i json con JAvascript

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
 include __DIR__ . "/includes/data/data.php";
-
 ?>
 
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
+Vue.config.devtools = true;
+
 new Vue({
-    el: '#app',
-    data: {     
-    }, 
+    el: '#root',
+    data: {   
+              dischi: [],
+    },
+    mounted() {
+        axios.get('localhost:8888/php-ajax-dischi/api/dischi.php').then((res) => {
+            this.dischi = res.data;
+        })
+    }
 });
